8f24aea4a238d9e81ad624e07aa71fdd8de22452
Osman S. Hasan <54463882+osman-s@users.noreply.github.com>
2020-01-23 15:19:44 -0500

    Finshed classes overview

--- first_world/classes.py
+++ first_world/classes.py
@@ -9,25 +9,36 @@
 # 4. Class attributes vs. instance attributes
 # 5. Class attributes vs. instance methods
 # 6. Magic Methods
+
+from abc import ABC, abstractmethod
+
+
 class Point:
     default_color = "red"
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
     def __str__(self):
         return f"({self.x}, {self.y})"
+
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
+
     def __gt__(self, other):
         return self.x > other.x and self.y > other.y
+
     def __add__(self, other):
         return Point(self.x + other.x, self.y + other.y)
-    @classmethod    
+
+    @classmethod
     def zero(cls):
-        return cls(0,0)
+        return cls(0, 0)
 
     def draw(self):
         print(f"Point ({self.x}, {self.y})")
+
 
 point = Point(1, 2)
 # print(type(point))
@@ -42,8 +53,10 @@ other = Point(1, 2)
 print(point == other)
 
 # 8. Performing Arithmetic Operations
-print( point + other)
+print(point + other)
 # 9. Making Custom Containers
+
+
 class TagCloud:
     def __init__(self):
         self.__tags = {}
@@ -63,6 +76,7 @@ class TagCloud:
     def __iter__(self):
         return iter(self.__tags)
 
+
 cloud = TagCloud()
 cloud["python"] = 10
 len(cloud)
@@ -74,23 +88,137 @@ cloud.add("python")
 # cloud["python"] = 10
 # for tag in cloud:
 #     print(tag)
-# 10. Private Members 
+# 10. Private Members
 # print(cloud.__tags["PYTHON"])
 print(cloud.__dict__)
 
 # 11. Properties
+
+
 class Product:
     def __init__(self, price):
         self.price = price
+
     @property
     def price(self):
         return self.__price
+
     @price.setter
     def price(self, value):
         if value < 0:
             raise ValueError("Price cannot be negative.")
         self.__price = value
 
+
 product = Product(10)
 # product.price = -1
 print(product.price)
+# 13. Inheritance
+
+
+class Animal:
+    def __init__(self):
+        print("Animal Constructor")
+        self.age = 1
+
+    def eat(self):
+        print("eat")
+
+
+class Mammal(Animal):
+    def __init__(self):
+        super().__init__()
+        print("Mammal Constructor")
+        self.weight = 2
+
+    def walk(self):
+        print("walk")
+
+
+class Fish(Animal):
+    def walk(self):
+        print("swim")
+
+
+m = Mammal()
+m.eat()
+print(m.age)
+
+print(isinstance(m, object))
+o = object()
+print(issubclass(Mammal, Animal))
+# 14. Method Overriding
+print(m.age)
+print(m.weight)
+# 15. Multi-level Inheritance
+
+
+class Bird(Animal):
+    def fly(self):
+        print("fly")
+
+
+class Chicken(Bird):
+    pass
+# Limit inherticance to 1 or 2 levels. To solve business solutions
+# 16. Multiple Ineritance
+
+
+class Employee:
+    def greet(self):
+        print("Employee Greet")
+
+
+class Person:
+    def greet(self):
+        print("Person Greet")
+
+
+class Manager(Employee, Person):  # looks at first base class
+    pass
+
+
+manage = Manager()
+manage.greet()
+# 17. A good example of inheritance
+
+
+class InvalidOperationError(Exception):
+    pass
+
+
+class Stream(ABC):
+    def __init__(self):
+        self.opened = False
+
+    def open(self):
+        if self.opened:
+            raise InvalidOperationError("Stream is already open")
+        self.opened = True
+
+    def close(self):
+        if not self.opened:
+            raise InvalidOperationError("Stream is already closed")
+        self.opened = False
+
+    @abstractmethod
+    def read(self):
+        pass
+
+
+class FileStream(Stream):
+    def read(self):
+        print("Reading data from a file")
+
+
+class NetworkStream(Stream):
+    def read(self):
+        print("Reading data from a network")
+
+class MemoryStream(Stream):
+    pass #considered abract method 
+# 18. Abstract Base Class - provide common code to dervs.
+stream = MemoryStream()
+stream.open()
+
+
